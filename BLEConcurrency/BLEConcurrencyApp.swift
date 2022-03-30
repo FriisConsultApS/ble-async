51135a8d5cd25d91e332dd2d8caa2b721a85a878
Per Friis <per.friis@friisconsult.com>
2022-03-30 10:13:39 +0200

    Initial Commit

---
+++ BLEConcurrency/BLEConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEConcurrencyApp.swift
+//  BLEConcurrency
+//
+//  Created by Per Friis on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BLEConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEConcurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLEConcurrency
+//
+//  Created by Per Friis on 30/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
